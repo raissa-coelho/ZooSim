f2d8e4156affbb5cb0364ea0a91696f9f920d855
Matheus S. Menezes <matheus17menezes@gmail.com>
2023-05-03 22:52:06 -0300

    Finished most of the initializers in data.c

--- data/data.c
+++ data/data.c
@@ -37,3 +37,58 @@ Animal* criar_animais(int tipo, int quantidade) {
 
     return animais;
 }
+
+Comedouro inicializa_comedouro(int tipo_alimento, float capacidade_max) {
+    Comedouro comedouro;
+    comedouro.tipo_alimento = tipo_alimento;
+    comedouro.qtd_alimento_disp = 0;
+    comedouro.capacidade_max = capacidade_max;
+    return comedouro;
+}
+
+Estoque inicializa_estoque(float carne_disp, float vegetais_disp, float frutas_disp) {
+    Estoque estoque;
+    estoque.carne_disp = carne_disp;
+    estoque.vegetais_disp = vegetais_disp;
+    estoque.frutas_disp = frutas_disp;
+    // inicializa outros tipos de alimentos
+    return estoque;
+}
+
+// Inicializa uma lista de veterinários vazia
+Veterinario* inicializa_lista_veterinarios(int tamanho) {
+    Veterinario* lista = malloc(sizeof(Veterinario) * tamanho);
+    if (lista == NULL) {
+        printf("Erro ao alocar memória para lista de veterinários\n");
+        exit(1);
+    }
+    for (int i = 0; i < tamanho; i++) {
+        lista[i].id = 0;
+        lista[i].comedouros_resp = NULL;
+        lista[i].num_comedouros_resp = 0;
+    }
+    return lista;
+}
+
+// Inicializa e aloca memória para um veterinário
+void inicializa_veterinario(Veterinario* vet, int id, int num_comedouros_resp, int* comedouros_resp) {
+    vet->id = id;
+    vet->num_comedouros_resp = num_comedouros_resp;
+    vet->comedouros_resp = malloc(sizeof(int) * num_comedouros_resp);
+    if (vet->comedouros_resp == NULL) {
+        printf("Erro ao alocar memória para lista de comedouros responsáveis do veterinário\n");
+        exit(1);
+    }
+    for (int i = 0; i < num_comedouros_resp; i++) {
+        vet->comedouros_resp[i] = comedouros_resp[i];
+    }
+}
+
+Fornecedor inicializa_fornecedor(float carne_entregue, float vegetais_entregues, float frutas_entregues) {
+    Fornecedor fornecedor;
+    fornecedor.carne_entregue = carne_entregue;
+    fornecedor.vegetais_entregues = vegetais_entregues;
+    fornecedor.frutas_entregues = frutas_entregues;
+    // inicializa outros tipos de alimentos entregues
+    return fornecedor;
+}
